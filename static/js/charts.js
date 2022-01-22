08eb24c32a33641c82057c3c3f928d449be147ce
Andrés Pombo <pombo.andres90@gmail.com>
2022-01-21 23:43:11 -0500

    added bar chart and updated css.

--- static/js/charts.js
+++ static/js/charts.js
@@ -58,30 +58,52 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
+    var result = resultArray[0]
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otuIds = result.otu_ids;
+    var otuLabels = result.otu_labels;
+    var sampleValues = result.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    // var yticks = otuIds.map(id => parseInt(id.otu_ids)).sort((a,b) => a.otu_ids - b.otu_ids).reverse().slice(0,10)
+    var yticks = otuIds.slice(0,10).map(id => `OTU ${id}`).reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      y: yticks,
+      x: sampleValues.slice(0,10).reverse(),
+      type: "bar",
+      text: otuLabels,
+      orientation: 'h',
+      marker: {
+        color: 'rgb(180, 205, 152)',
+        line: {
+          color: 'rgb(180, 215, 152)',
+          width: 1
+        }
+      }
+
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      font: {
+        family: 'Arial',
+        size: 12,
+      }
+      
+
      
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }
